5e1a66e0c0a67065bba6c831debc104233a71631
Алексей Шкарупа <shkarupa.alex@gmail.com>
2017-08-16 01:06:07 +0300

    Модуль переписан на основе bemfs-loader под новый подход и с оглядкой на производительность

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-module.exports = require('./lib/loader');
+const path = require('path');
+const loaderUtils = require('loader-utils');
+const nodeEval = require('node-eval');
+const resolveFS = require('./lib/resolve-fs');
+
+/**
+ * BemDeps loader
+ *
+ * @param {String} source
+ */
+function bemDepsLoader(source) {
+  const callback = this.async();
+  const options = {
+    stringify: true,
+    levels: [],
+    techMap: {},
+  };
+  Object.assign(options, loaderUtils.getOptions(this));
+
+  const bemDeps = nodeEval(source);
+
+  const self = this;
+  resolveFS(bemDeps, options.techMap, options.levels).then((result) => {
+    const dependencies = result.found.concat(result.checked, result.skipped);
+    dependencies.forEach((fileName) => {
+      self.addDependency(path.resolve(fileName));
+    });
+
+    callback(null, 'module.exports = ' + JSON.stringify(result.found) + ';');
+  }).catch(callback);
+}
+
+module.exports = bemDepsLoader;
